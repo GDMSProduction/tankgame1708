c8e50ee4eb52afa83ae08ce075be35f17333d37b
Matthew Louchart <mnlouchart@yahoo.com>
2018-07-24 19:37:20 -0400

    Scripts

--- TanksGame/Assets/Scripts/Tank/TankShooting.cs
+++ TanksGame/Assets/Scripts/Tank/TankShooting.cs
@@ -45,40 +45,6 @@ public class TankShooting : MonoBehaviour
 
     private void Update()
     {
-<<<<<<< HEAD
-        // The slider should have a default value of the minimum launch force.
-        m_AimSlider.value = m_MinLaunchForce;
-
-        // If the max force has been exceeded and the shell hasn't yet been launched...
-        if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired)
-        {
-            // ... use the max force and launch the shell.
-            m_CurrentLaunchForce = m_MaxLaunchForce;
-            Fire();
-        }
-
-        // Otherwise, if the fire button has just started being pressed...
-        //else if (Input.GetButtonDown(m_FireButton))
-        else if (CrossPlatformInputManager.GetButtonDown(m_FireButton))
-        {
-            // ... reset the fired flag and reset the launch force.
-            m_Fired = false;
-            m_CurrentLaunchForce = m_MinLaunchForce;
-
-            // Change the clip to the charging clip and start it playing.
-            m_ShootingAudio.clip = m_ChargingClip;
-            m_ShootingAudio.Play();
-        }
-
-        // Otherwise, if the fire button is being held and the shell hasn't been launched yet...
-        //else if (Input.GetButton(m_FireButton) && !m_Fired)
-        else if (CrossPlatformInputManager.GetButton(m_FireButton) && !m_Fired)
-        {
-            // Increment the launch force and update the slider.
-            m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
-
-            m_AimSlider.value = m_CurrentLaunchForce;
-=======
        
 
         if (GameManager.IsSinglePlayer && m_PlayerNumber == 1)
@@ -127,17 +93,10 @@ public class TankShooting : MonoBehaviour
                 Fire();
                
             }
->>>>>>> 520f73c970b565165509ada2528a032e97fe1f95
         }
 
-        // Otherwise, if the fire button is released and the shell hasn't been launched yet...
-        //else if (Input.GetButtonUp(m_FireButton) && !m_Fired)
-        else if (CrossPlatformInputManager.GetButtonUp(m_FireButton) && !m_Fired)
+        if (!GameManager.IsSinglePlayer)
         {
-<<<<<<< HEAD
-            // ... launch the shell.
-            Fire();
-=======
            
             // The slider should have a default value of the minimum launch force.
             m_AimSlider.value = m_MinLaunchForce;
@@ -192,9 +151,7 @@ public class TankShooting : MonoBehaviour
                 
                
             }
->>>>>>> 520f73c970b565165509ada2528a032e97fe1f95
         }
-        
     }
 
     //private IEnumerator delayer()
