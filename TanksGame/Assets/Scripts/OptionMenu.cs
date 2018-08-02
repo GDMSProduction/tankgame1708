d71464ddc66a4ec95747098b1683d9a005938d11
Dylen Almonte <daalmonte@student.fullsail.edu>
2018-08-02 16:01:47 -0400

    did audio and options started

---
+++ TanksGame/Assets/Scripts/OptionMenu.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class OptionMenu : MonoBehaviour {
+
+    public AudioMixer beatbox;
+
+    public Dropdown resolutiondropdown;
+
+    Resolution[] resolutions;
+    // Use this for initialization
+    void Start() {
+        resolutions = Screen.resolutions;
+
+        resolutiondropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentresolution = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentresolution = i;
+            }
+        }
+
+        resolutiondropdown.AddOptions(options);
+        resolutiondropdown.value = currentresolution;
+        resolutiondropdown.RefreshShownValue();
+
+    }
+    public void setresolution(int resolutionindex)
+        {
+        Resolution resolution = resolutions[resolutionindex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+
+}
+
+        public void setaudio(float volume)
+    {
+        beatbox.SetFloat("volume", volume);
+    }
+
+
+        public void setquality(int qualityindex)
+    {
+        QualitySettings.SetQualityLevel(qualityindex);
+    }
+
+        public void SetFullScreen(bool isfullscreen)
+    {
+        Screen.fullScreen = isfullscreen;
+    }
+
+}
